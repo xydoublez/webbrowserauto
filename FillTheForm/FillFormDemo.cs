5a17bf32a99b76f8999fa67780e498af36afcc23
李志强 <love@lizhiqiang.name>
2017-06-13 09:10:46 +0800

    添加项目文件。

---
+++ FillTheForm/FillFormDemo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FillTheForm
+{
+    public partial class FillFormDemo : Form
+    {
+        public FillFormDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            this.wb.Navigate("http://oa.msunsoft.com:8088/quexian/web/login.aspx");
+            count = 1;
+        }
+
+        private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var script = @" function myFunc(name,pass){
+                                setTimeout(function(){
+       	                         username.value = name;
+                                 password.value = pass;
+                                 //alert(name);
+                                  alert($('#password').val());
+                                 },100);
+                               }
+";
+            InstallScript(script);
+            var r = this.wb.Document.InvokeScript("myFunc", new object[] { this.v1.Text, this.v2.Text });
+            //this.timer1.Start();
+            
+        }
+
+        static int count = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count == 1) {
+                //this.wb.Document.GetElementById("username").SetAttribute("value", this.v1.Text);
+                this.wb.Document.GetElementById("password").SetAttribute("value", this.v2.Text);
+                //点击登录按扭
+                //this.wb.Document.GetElementById("password").Parent.Parent.NextSibling.Children[0].Children[0].InvokeMember("click");
+                
+                count += 1;
+            }
+        
+            if (count > 3)
+            {
+                this.timer1.Stop();
+            }
+        }
+        //添加脚本
+        private void InstallScript(string code)
+        {
+
+            if (null == this.wb.Document || ( string.IsNullOrEmpty(code)))
+                return;
+            HtmlElement scriptElement = null;
+            if (null == scriptElement)
+            {
+                scriptElement = this.wb.Document.CreateElement("script");
+
+                HtmlElementCollection elements = this.wb.Document.GetElementsByTagName("head");
+
+                if (elements.Count > 0)
+                    this.wb.Document.GetElementsByTagName("head")[0].AppendChild(scriptElement);
+
+            }
+
+            //scriptElement.SetAttribute("id", id);
+            scriptElement.SetAttribute("type", "text/javascript");
+            scriptElement.SetAttribute("language", "javascript");
+            scriptElement.SetAttribute("text", code);
+          
+
+        }
+    }
+}
---
+++ TestFillTheForm/Form1.cs
@@ -0,0 +1,31 @@
+﻿using FillTheForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestFillTheForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillFormDemo ffd = new FillFormDemo();
+            ffd.Show();
+            //webBrowser1.Navigate("http://oa.msunsoft.com:8088/quexian/web/login.aspx");
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.webBrowser1.Document.GetElementById("username").InnerText = "测试";
+            this.webBrowser1.Document.GetElementById("password").SetAttribute("value", "password");
+        }
+    }
+}
