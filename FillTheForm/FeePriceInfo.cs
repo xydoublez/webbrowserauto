b4e30d2b069452af17241f2194fd94fc1282bfe1
李志强 <love@lizhiqiang.name>
2017-06-14 09:30:44 +0800

    dd

--- FillTheForm/FeePriceInfo.cs
+++ FillTheForm/FeePriceInfo.cs
@@ -6,36 +6,117 @@ namespace FillTheForm
 {
     public class FeePriceInfo
     {
+        /// <summary>
+        /// 身份证号
+        /// </summary>
         public string queryCodeTxt { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
         public string patientNameTxt { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
         public string sexcmb { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
         public string ageTxt { get; set; }
+        /// <summary>
+        /// 帐户余额
+        /// </summary>
         public string freeCost { get; set; }
+        /// <summary>
+        /// 结算类别如医保
+        /// </summary>
         public string cmbPayKind { get; set; }
+        /// <summary>
+        /// 医疗证号
+        /// </summary>
         public string mcardNoTxt { get; set; }
+        /// <summary>
+        /// 医疗类别如普通门诊
+        /// </summary>
         public string medicalTypeCmb { get; set; }
+        /// <summary>
+        /// 门诊诊断
+        /// </summary>
         public string diagnoseCmb { get; set; }
+        /// <summary>
+        /// 看诊科室
+        /// </summary>
         public string deptCmb { get; set; }
+        /// <summary>
+        /// 看诊医生
+        /// </summary>
         public string docCmb { get; set; }
-        public Feeprice[] FeePrices { get; set; }
+        /// <summary>
+        /// 费用明细列表
+        /// </summary>
+        public List<FeePrice> FeePriceList { get; set; }
     }
 
-    public class Feeprice
+    public class FeePrice
     {
+        /// <summary>
+        /// 品名
+        /// </summary>
         public string pinMing { get; set; }
+        /// <summary>
+        /// 规格
+        /// </summary>
         public string guiGe { get; set; }
+        /// <summary>
+        /// 批次
+        /// </summary>
         public string piCi { get; set; }
-        public string lingShouJian { get; set; }
+        /// <summary>
+        /// 零售价
+        /// </summary>
+        public string lingShouJia { get; set; }
+        /// <summary>
+        /// 包装单位
+        /// </summary>
         public string baoZhuangDanWei { get; set; }
+        /// <summary>
+        /// 数量
+        /// </summary>
         public string shuLiang { get; set; }
+        /// <summary>
+        /// 计价单位
+        /// </summary>
         public string jiJiaDanWei { get; set; }
+        /// <summary>
+        /// 金额
+        /// </summary>
         public string jinE { get; set; }
+        /// <summary>
+        /// 基本用量
+        /// </summary>
         public string jiBenYongLiang { get; set; }
+        /// <summary>
+        /// 计量单位
+        /// </summary>
         public string jiLiangDanWei { get; set; }
+        /// <summary>
+        /// 用法
+        /// </summary>
         public string yongFa { get; set; }
+        /// <summary>
+        /// 频次
+        /// </summary>
         public string pinCi { get; set; }
+        /// <summary>
+        /// 付数
+        /// </summary>
         public string fuShu { get; set; }
+        /// <summary>
+        /// 项目等级
+        /// </summary>
         public string xiangMuDengJi { get; set; }
+        /// <summary>
+        /// 执行科室
+        /// </summary>
         public string zhiXingKeShi { get; set; }
     }
 }
--- FillTheForm/FillFormDemo.cs
+++ FillTheForm/FillFormDemo.cs
@@ -39,7 +39,7 @@ namespace FillTheForm
             //门诊医生开方界面
             if (this.wb.Document.Url.LocalPath.IndexOf("feeprice.jsp") > -1)
             {
-
+                AutoFeePrice(this.wb.Document);
             }
            
 
@@ -52,21 +52,89 @@ namespace FillTheForm
             var script = @" function zyAutoFeePrice(jsonInfo){
 　　　　　　　　　　　　　　　　var feeChargePriceJson = eval('('+jsonInfo+')');
                                 setTimeout(function(){
+                                   //1基本信息赋值
                                    unieap.byId('queryCodeTxt').setValue(feeChargePriceJson.queryCodeTxt);
-                                   unieap.byId('queryCodeTxt').setValue(feeChargePriceJson.queryCodeTxt);
+                                   unieap.byId('patientNameTxt').setValue(feeChargePriceJson.patientNameTxt);
+                                   unieap.byId('sexcmb').setValue(feeChargePriceJson.sexcmb);
+                                   unieap.byId('ageTxt').setValue(feeChargePriceJson.ageTxt);
+                                   unieap.byId('freeCost').setValue(feeChargePriceJson.freeCost);
+                                   unieap.byId('cmbPayKind').setValue(feeChargePriceJson.cmbPayKind);
+                                   unieap.byId('mcardNoTxt').setValue(feeChargePriceJson.mcardNoTxt);
+                                   unieap.byId('medicalTypeCmb').setValue(feeChargePriceJson.medicalTypeCmb);
+                                   unieap.byId('diagnoseCmb').setValue(feeChargePriceJson.diagnoseCmb);
+                                   unieap.byId('deptCmb').setValue(feeChargePriceJson.deptCmb);
+                                   unieap.byId('docCmb').setValue(feeChargePriceJson.docCmb);
+                                  //2费用明细赋值
+                                  var feePrices = feeChargePriceJson.feePrices;
+                                  for(int i=0;i<feePrices.length;i++){
+                                      var fee = feePrices[i];
+                                      //fee.pinMing;
+                                  }
                                  },100);
                                }
 ";
             InstallScript(script);
 
-            ////正式查询数据库给feePriceInfo
-            //FeePriceInfo feePriceInfo = new FeePriceInfo();
-            ////身份证号
-            //feePriceInfo.queryCodeTxt = "37148290000000";
-            ////其他字段．．．．
-            //string feeChargePriceJson = Newtonsoft.Json.JsonConvert.SerializeObject(feePriceInfo);
+
             //测试用
-            string feeChargePriceJson = System.IO.File.ReadAllText("data.json");
+            //string feeChargePriceJson = System.IO.File.ReadAllText("data.json");
+
+            ////正式查询数据库给feePriceInfo
+            FeePriceInfo feePriceInfo = new FeePriceInfo();
+            //基本信息
+            feePriceInfo.queryCodeTxt = "372826193205043124";
+            feePriceInfo.patientNameTxt = "宋立玉";
+            feePriceInfo.sexcmb = "女";
+            feePriceInfo.ageTxt = "85";
+            feePriceInfo.freeCost = "0.00";
+            feePriceInfo.cmbPayKind = "医保";
+            feePriceInfo.mcardNoTxt = "371102464910";
+            feePriceInfo.medicalTypeCmb = "普通门诊";
+            feePriceInfo.diagnoseCmb = "感冒";
+            feePriceInfo.deptCmb = "外科门诊";
+            feePriceInfo.docCmb = "门诊医师";
+            //费用明细
+            List<FeePrice> FeePriceList = new List<FeePrice>();
+            //真实取数据库for添加列表
+            FeePrice feePrice = new FeePrice();
+            feePrice.pinMing = "无痛皮试";
+            feePrice.guiGe = "";
+            feePrice.piCi = "";
+            feePrice.lingShouJia = "8";
+            feePrice.baoZhuangDanWei = "次";
+            feePrice.shuLiang = "1";
+            feePrice.jiJiaDanWei = "次";
+            feePrice.jinE = "8.00";
+            feePrice.jiBenYongLiang = "";
+            feePrice.jiLiangDanWei = "";
+            feePrice.yongFa = "";
+            feePrice.pinCi = "";
+            feePrice.fuShu = "";
+            feePrice.xiangMuDengJi = "甲类";
+            feePrice.zhiXingKeShi = "外科门诊";
+            FeePriceList.Add(feePrice);
+
+            FeePrice feePrice1 = new FeePrice();
+            feePrice1.pinMing = "破伤风抗毒素注射液";
+            feePrice1.guiGe = "1500u*10支";
+            feePrice1.piCi = "000000";
+            feePrice1.lingShouJia = "30";
+            feePrice1.baoZhuangDanWei = "盒";
+            feePrice1.shuLiang = "1";
+            feePrice1.jiJiaDanWei = "支";
+            feePrice1.jinE = "30.00";
+            feePrice1.jiBenYongLiang = "";
+            feePrice1.jiLiangDanWei = "u";
+            feePrice1.yongFa = "";
+            feePrice1.pinCi = "";
+            feePrice1.fuShu = "";
+            feePrice1.xiangMuDengJi = "甲类";
+            feePrice1.zhiXingKeShi = "外科门诊";
+            FeePriceList.Add(feePrice1);
+
+            feePriceInfo.FeePriceList = FeePriceList;
+
+            string feeChargePriceJson = Newtonsoft.Json.JsonConvert.SerializeObject(feePriceInfo);
             var r = this.wb.Document.InvokeScript("zyAutoFeePrice", new object[] { feeChargePriceJson });
         }
         /// <summary>
